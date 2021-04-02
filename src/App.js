9fcca550d29f581bc074ea50982d749e3b9bbb92
Arjun Singha <singha.arjun@gmail.com>
2021-04-02 11:41:03 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './default.scss';
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
